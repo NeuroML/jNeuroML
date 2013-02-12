274a547c3e72f03f2cac5c4f4bb5ab6ac8f13ff9
Padraig Gleeson <p.gleeson@gmail.com>
2013-02-12 12:14:29 +0000

    Initial version of jNeuroML maven based project

---
+++ src/main/java/org/neuroml/JNeuroML.java
@@ -0,0 +1,13 @@
+package org.neuroml;
+
+/**
+ * Hello world!
+ *
+ */
+public class JNeuroML 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello from jNeuroML!" );
+    }
+}
---
+++ src/test/java/org/neuroml/JNeuroMLTest.java
@@ -0,0 +1,38 @@
+package org.neuroml;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class JNeuroMLTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public JNeuroMLTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( JNeuroMLTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
