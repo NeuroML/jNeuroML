ce7f1c79cc177c2ce77a0f3462e1160d7c5c7339
Padraig Gleeson <p.gleeson@gmail.com>
2013-03-12 15:33:12 +0000

    Adding initial export to XPP

--- src/main/java/org/neuroml/JNeuroML.java
+++ src/main/java/org/neuroml/JNeuroML.java
@@ -11,10 +11,15 @@ import org.lemsml.jlems.core.run.RuntimeError;
 import org.lemsml.jlems.core.sim.ContentError;
 import org.lemsml.jlems.core.sim.ParseException;
 import org.lemsml.jlems.core.type.BuildException;
+import org.lemsml.jlems.core.type.Lems;
 import org.lemsml.jlems.core.xml.XMLException;
 import org.lemsml.jlems.io.Main;
 import org.lemsml.jlems.io.logging.DefaultLogger;
 import org.lemsml.jlems.io.out.FileResultWriterFactory;
+import org.lemsml.jlems.io.util.FileUtil;
+import org.neuroml.export.AppTest;
+import org.neuroml.export.Utils;
+import org.neuroml.export.xpp.XppWriter;
 import org.neuroml.model.util.NeuroML2Validator;
 import org.lemsml.jlems.viz.datadisplay.SwingDataViewerFactory;
 import org.xml.sax.SAXException;
@@ -30,14 +35,18 @@ public class JNeuroML {
 	public static String HELP_FLAG = "-help";
 	public static String HELP_FLAG_SHORT = "-h";
 	public static String HELP_FLAG_SHORT_Q = "-?";
-	
+
 	public static String VALIDATE_FLAG = "-validate";
 	
+	public static String XPP_FLAG = "-xpp";
+	
 	static String usage = "Usage: \n\n" +
-            "    "+JNML_SCRIPT+" "+VALIDATE_FLAG+" NMLFile.nml\n" +
-            "           Validate NMLFile.nml against latest v2beta Schema & perform a number of other tests\n\n"+
             "    "+JNML_SCRIPT+" LEMSFile.xml\n" +
             "           Load LEMSFile.xml using jLEMS, parse it and validate it as LEMS, and execute the model it contains\n\n"+
+            "    "+JNML_SCRIPT+" LEMSFile.xml "+XPP_FLAG+"\n" +
+            "           Load LEMSFile.xml using jLEMS, and convert it to XPP format\n\n"+
+            "    "+JNML_SCRIPT+" "+VALIDATE_FLAG+" NMLFile.nml\n" +
+            "           Validate NMLFile.nml against latest v2beta Schema & perform a number of other tests\n\n"+
             "    "+JNML_SCRIPT+" "+HELP_FLAG+"\n" +
             "    "+JNML_SCRIPT+" "+HELP_FLAG_SHORT+"\n" +
             "    "+JNML_SCRIPT+" "+HELP_FLAG_SHORT_Q+"\n" +
@@ -47,73 +56,106 @@ public class JNeuroML {
 		System.out.println(usage);
 	}
 
-	public static void main(String[] args) throws SAXException, IOException, JAXBException {
-		if (args.length == 0) {
-			System.err.println("Error, no arguments to "+JNML_SCRIPT);
-			showUsage();
-			System.exit(1);
-		} else if (args.length == 1) {
-			if (args[0].startsWith("-")) {
-				if (args[0].equals(HELP_FLAG) || args[0].equals(HELP_FLAG_SHORT) || args[0].equals(HELP_FLAG_SHORT_Q)) {
-					showUsage();
-					System.exit(0);
+	public static void main(String[] args) {
+		try {
+			if (args.length == 0) {
+				System.err.println("Error, no arguments to "+JNML_SCRIPT);
+				showUsage();
+				System.exit(1);
+			} else if (args.length == 1) {
+				if (args[0].startsWith("-")) {
+					if (args[0].equals(HELP_FLAG) || args[0].equals(HELP_FLAG_SHORT) || args[0].equals(HELP_FLAG_SHORT_Q)) {
+						showUsage();
+						System.exit(0);
+					} else {
+						System.err.println("Unrecognised argument: "+args[0]);
+						showUsage();
+						System.exit(1);
+					}
 				} else {
-					System.err.println("Unrecognised argument: "+args[0]);
-					showUsage();
-					System.exit(1);
-				}
-			} else {
-				File lemsFile = new File(args[0]);
-				if (!lemsFile.exists()) {
-					System.err.println("File does not exist: "+args[0]);
-					showUsage();
-					System.exit(1);
-				}
-
-				System.out.println("Loading: "+lemsFile.getAbsolutePath()+" with jLEMS...");
-		    	FileResultWriterFactory.initialize();
-		    	SwingDataViewerFactory.initialize();
-				DefaultLogger.initialize();
-				
-				try {
+					File lemsFile = new File(args[0]);
+					if (!lemsFile.exists()) {
+						System.err.println("File does not exist: "+args[0]);
+						showUsage();
+						System.exit(1);
+					}
+	
+					System.out.println("Loading: "+lemsFile.getAbsolutePath()+" with jLEMS...");
+			    	FileResultWriterFactory.initialize();
+			    	SwingDataViewerFactory.initialize();
+					DefaultLogger.initialize();
+					
 					Main.main(args);
-				} catch (ConnectionError e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (ContentError e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (RuntimeError e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (ParseError e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (ParseException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (BuildException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (XMLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+					
 				}
 				
-			}
-			
-		} else if (args.length == 2) {
-			if  (args[0].equals(VALIDATE_FLAG))
-			{
-				File xmlFile = new File(args[1]);
-				if (!xmlFile.exists()) {
-					System.err.println("File does not exist: "+args[1]);
+			} else if (args.length == 2) {
+				if  (args[0].equals(VALIDATE_FLAG)) {
+					File xmlFile = new File(args[1]);
+					if (!xmlFile.exists()) {
+						System.err.println("File does not exist: "+args[1]);
+						showUsage();
+						System.exit(1);
+					}
+					NeuroML2Validator nmlv =  new NeuroML2Validator();
+					nmlv.validateWithTests(xmlFile);
+				} else if (args[1].equals(XPP_FLAG)) {
+					File lemsFile = new File(args[0]);
+					if (!lemsFile.exists()) {
+						System.err.println("File does not exist: "+args[0]);
+						showUsage();
+						System.exit(1);
+					}
+			        
+					Lems lems = Utils.loadLemsFile(lemsFile);
+	
+					XppWriter xppw = new XppWriter(lems);
+			        String ode = xppw.getMainScript();
+	
+			        File odeFile = new File(lemsFile.getParentFile(),lemsFile.getName().replaceAll(".xml", ".ode"));
+			        System.out.println("Writing to: "+odeFile.getAbsolutePath());
+			        
+			        FileUtil.writeStringToFile(ode, odeFile);
+					
+				} else {
+					System.err.println("Unrecognised arguments: "+args[0]+" "+args[1]);
 					showUsage();
 					System.exit(1);
+					
 				}
-				NeuroML2Validator nmlv =  new NeuroML2Validator();
-				nmlv.validateWithTests(xmlFile);
 			}
-		}
+		
+		} catch (ConnectionError e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ContentError e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (RuntimeError e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseError e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BuildException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (XMLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JAXBException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
 	}
+	
 }
