43a4f19f2ec504d67a547fd3f03770c8ff9fd2a6
Padraig Gleeson <p.gleeson@gmail.com>
2017-05-31 10:05:39 +0100

    Update to v0.8.2

--- jnml.bat
+++ jnml.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set JNML_VERSION=0.8.1
+set JNML_VERSION=0.8.2
 
 set CLASSPATH=target\jNeuroML-%JNML_VERSION%-jar-with-dependencies.jar;%JNML_HOME%\jNeuroML-%JNML_VERSION%-jar-with-dependencies.jar
 
