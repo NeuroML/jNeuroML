4081185185a24440d84998ecaf8eaabd8de354b4
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2020-06-20 20:10:56 +0100

    Whitespace fixes

--- getNeuroML.py
+++ getNeuroML.py
@@ -5,6 +5,7 @@ import sys
 import os.path as op
 import subprocess
 
+
 def main():
     """Main"""
     mode = "update"
@@ -50,9 +51,7 @@ def main():
     # Which repos use a development branch?
     dev_branch_repos = neuroml2_spec_repo + neuroml_repos + jlems_repo
 
-
-    all_repos = lems_repos  + neuroml_repos 
-
+    all_repos = lems_repos + neuroml_repos
 
     # Set the preferred method for cloning from GitHub
     github_pref = "HTTP"
@@ -69,22 +68,21 @@ def main():
         local_dir = ".." + os.sep + repo.split("/")[1]
 
         if mode is "clean":
-            print("------ Cleaning: %s -------"%repo)
+            print("------ Cleaning: %s -------" % repo)
             if repo in java_repos:
                 command = "mvn clean"
                 print("It's a Java repository, so cleaning using Maven...")
                 info = execute_command_in_dir(command, local_dir)
 
         if mode is "update":
-
-            print("------ Updating: %s -------" %repo)
+            print("------ Updating: %s -------" % repo)
 
             runMvnInstall = False
 
             if not op.isdir(local_dir):
                 command = "git clone %s%s" % (pre_gh[github_pref], repo)
-                print ("Creating a new directory: %s by cloning from GitHub" % \
-                    (local_dir))
+                print("Creating a new directory: %s by cloning from GitHub" %
+                      (local_dir))
                 execute_command_in_dir(command, "..")
 
                 runMvnInstall = True
@@ -92,7 +90,7 @@ def main():
             if switch_to_branch:
                 if (repo in dev_branch_repos):
                     command = "git checkout %s" % (switch_to_branch)
-                    print ("Switching to branch: %s" % (switch_to_branch))
+                    print("Switching to branch: %s" % (switch_to_branch))
                     exit_on_fail = switch_to_branch is not "experimental"
                     execute_command_in_dir(command, local_dir, exit_on_fail)
                     runMvnInstall = True
@@ -112,34 +110,29 @@ def main():
                 print("It's a Java repository, so installing using Maven...")
                 info = execute_command_in_dir(command, local_dir)
 
-                #The code below needs a non trivial rewrite due to python3 differences.
-
-                #                
+                # The code below needs a non trivial rewrite due to python3 differences.
                 if str("BUILD SUCCESS") in str(info):
-                    print("Successful installation using : %s!" %command)
+                    print("Successful installation using : %s!" % command)
                 else:
-                    print("Problem installing using : %s!" %command)
+                    print("Problem installing using : %s!" % command)
                     print(info)
                     exit(1)
 
     if mode is "update":
         print("All repositories successfully updated & Java modules built!")
-        print("You should be able to run some examples straight " \
-              "away using jnml: ")
+        print("You should be able to run some examples straight away using jnml: ")
         if os.name is not 'nt':
-            print("  ./jnml "\
-                "-validate ../NeuroML2/examples/NML2_FullNeuroML.nml")
-            print("  ./jnml " \
-                "../NeuroML2/LEMSexamples/LEMS_NML2_Ex2_Izh.xml")
+            print("  ./jnml " +
+                  "-validate ../NeuroML2/examples/NML2_FullNeuroML.nml")
+            print("  ./jnml " +
+                  "../NeuroML2/LEMSexamples/LEMS_NML2_Ex2_Izh.xml")
         else:
-            print("  jnml -validate " \
-                "..\\NeuroML2\\examples\\NML2_FullNeuroML.nml")
-            print("  jnml " \
-                "..\\NeuroML2\\LEMSexamples\\LEMS_NML2_Ex2_Izh.xml")
+            print("  jnml -validate " +
+                  "..\\NeuroML2\\examples\\NML2_FullNeuroML.nml")
+            print("  jnml " +
+                  "..\\NeuroML2\\LEMSexamples\\LEMS_NML2_Ex2_Izh.xml")
     if mode is "clean":
         print("All repositories successfully cleaned!")
-
-
 
 
 def execute_command_in_dir(command, directory, exit_on_fail=True):
@@ -152,10 +145,10 @@ def execute_command_in_dir(command, directory, exit_on_fail=True):
     p = subprocess.Popen(command, cwd=directory, shell=True, stdout=subprocess.PIPE)
     return_str = p.communicate()
 
-    if p.returncode != 0:       
-        print("Error: %s" % p.returncode)          
+    if p.returncode != 0:
+        print("Error: %s" % p.returncode)
         print(return_str[0])
-        if exit_on_fail: 
+        if exit_on_fail:
             exit(p.returncode)
     if (sys.version_info > (3, 0)):
         return return_str[0].decode("utf-8")
@@ -167,7 +160,7 @@ def help_info():
     print("\nUsage:\n\n    python getNeuroML.py\n        " \
         "Pull (or clone) the latest version of all NeuroML 2 repos & " \
         "compile/install with Maven if applicable\n\n" \
-        "    python getNeuroML.py clean\n        " 
+        "    python getNeuroML.py clean\n        "
         "Run 'mvn clean' on all Java repos\n\n" \
         "    python getNeuroML.py master\n       " \
         "Switch all repos to master branch\n\n" \
