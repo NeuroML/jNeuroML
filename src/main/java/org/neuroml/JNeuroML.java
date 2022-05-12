94bd3733b6e894c545ecd9788dd8b3a0a759c370
Padraig Gleeson <p.gleeson@gmail.com>
2022-05-12 11:26:39 +0100

    To v0.12.0 here, v1.9.0 in others for NeuroML v2.3

--- src/main/java/org/neuroml/JNeuroML.java
+++ src/main/java/org/neuroml/JNeuroML.java
@@ -60,7 +60,7 @@ public class JNeuroML
 
     public static final String JNML_SCRIPT = "jnml";
 
-    public static final String JNML_VERSION = "0.11.1";
+    public static final String JNML_VERSION = "0.12.0";
 
     public static final String HELP_FLAG = "-help";
     public static final String HELP_FLAG_SHORT = "-h";
