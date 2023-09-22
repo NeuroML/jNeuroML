8dda32d9a853a6b551df1600c99db94c34dd29b4
Padraig Gleeson <p.gleeson@gmail.com>
2023-09-22 17:41:19 +0100

    To v0.13.0

--- jnml.bat
+++ jnml.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set JNML_VERSION=0.12.4
+set JNML_VERSION=0.13.0
 
 set CLASSPATH=target\jNeuroML-%JNML_VERSION%-jar-with-dependencies.jar;%JNML_HOME%\jNeuroML-%JNML_VERSION%-jar-with-dependencies.jar
 
