4f91af86dd1d7c6344a2492ca1fa346f44ff53a6
pgleeson <p.gleeson@gmail.com>
2024-08-19 12:27:57 +0100

    To v0.13.3

--- jnml.bat
+++ jnml.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set JNML_VERSION=0.13.2
+set JNML_VERSION=0.13.3
 
 set CLASSPATH=target\jNeuroML-%JNML_VERSION%-jar-with-dependencies.jar;%JNML_HOME%\jNeuroML-%JNML_VERSION%-jar-with-dependencies.jar
 
